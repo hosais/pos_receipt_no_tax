95b3f8325815dfbda6d1595c5d2afcbc38429dcc
CHIH JEN (Joseph) LEE <hosais@users.noreply.github.com>
2025-10-05 13:20:11 +0800

    Update manifest to new updated xml file

--- __manifest__.py
+++ __manifest__.py
@@ -5,15 +5,14 @@
     "summary": "Removes tax info from POS frontend receipt using JS/OWL template override",
     "author": "TZ hosais CJL",
     "website": "https://github.com/hosais/pos_receipt_no_tax_js",
-     "depends": [
+    "depends": [
         "point_of_sale",
         "ba_dcp_pos_extension"
     ],
     "assets": {
-        "point_of_sale.assets": [
-            "pos_receipt_no_tax_js/static/src/js/pos_receipt_no_tax.js",
-            "pos_receipt_no_tax_js/static/src/xml/pos_receipt_template.xml",
-        ]
+        "web.assets_qweb": [
+            "pos_receipt_no_tax_js/static/src/xml/OrderReceipt.xml",
+        ],
     },
     "installable": True,
     "application": False,
