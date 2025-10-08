9341bbe54dad48ca4fca5eaaa7588065dec8fe17
hosais <hosais@gmail.com>
2025-10-08 13:48:55 +0800

    remove Total Tax in history reprint

--- __manifest__.py
+++ __manifest__.py
@@ -1,10 +1,10 @@
 {
-    "name": "POS Receipt No Tax (JS Override)",
+    "name": "POS Receipt No Tax",
     "version": "15.0.1.0.0",
     "category": "Point of Sale",
-    "summary": "Removes tax info from POS frontend receipt using JS/OWL template override",
+    "summary": "Removes tax info from POS frontend receipt using OWL template override",
     "author": "TZ hosais CJL",
-    "website": "https://github.com/hosais/pos_receipt_no_tax_js",
+    "website": "https://github.com/hosais/pos_receipt_no_tax",
     "depends": [
         "point_of_sale",
         "ba_dcp_pos_extension"
@@ -12,6 +12,7 @@
     "assets": {
         "web.assets_qweb": [
             "pos_receipt_no_tax_js/static/src/xml/OrderReceipt.xml",
+            'pos_receipt_no_tax/static/src/xml/ba_dcp_history_button.xml',
         ],
     },
     "installable": True,
